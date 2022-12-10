fb8cdde77d84aea184c651a15d6caa5164df5c5f
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-10 04:51:48 +0000

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,19 @@
 */
 int main(void)
 {
-int digit1, digit2;
+int num1, num2;
 
-for (digit1 = 0; digit1 < 9; digit1++)
+for (num1 = 0; num1 < 9; num1++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-{
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
-if (digit1 == 8 && digit2 == 9)
-continue;
-putchar(',');
-putchar(' ');
-}
+	for (num2 = num1 + 1; num2 < 10; num2++)
+	{
+		putchar((num1 % 10) + '0');
+		putchar((num1 % 10) + '0');
+		if (digit1 == 8 && digit2 == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
 }
 putchar('\n');
 return (0);
