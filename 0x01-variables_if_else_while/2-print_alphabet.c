2cc6e36410ec5503ed8f3225c266e2fa281bda5a
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-10 01:46:22 +0000

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: "prints all alphabets"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	char ch = 'a';
+    while (ch <= 'z')
+    {
+        printf("%c", ch);
+        ch++;
+    }
+    printf("\n");
+	return (0);
+}
