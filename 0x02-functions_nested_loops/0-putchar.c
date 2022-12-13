cadc216291c02633f3f64c7c63332bee38b852de
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-13 12:11:56 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program prints _putchar to console
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", "_putchar");
+	return (0);
+}
