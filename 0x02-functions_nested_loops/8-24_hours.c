13c1f4a465ae4a33b803d6e3326d5292737522de
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-14 23:46:18 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints sign
+ * @n: takes input to function
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+
+	while (i < 24)
+	{
+		int j = 0;
+
+		while (j < 60)
+		{
+			printf("%02d:", i);
+			printf("%02d\n", j);
+			j++;
+		}
+		i++;
+	}
+}
