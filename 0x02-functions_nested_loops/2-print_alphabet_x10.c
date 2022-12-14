3b48aa683d4a9d00e92b7b2aa20ed4effff36fef
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-13 16:34:56 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - prints alphabets
+ * Description: program prints alphabets
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int c = 0;
+	char a = 'a';
+
+	for(c = 0; c < 10; c++)
+	{
+
+		a = 'a';
+		while (a <= 'z')
+		{
+			putchar(a);
+			a++;
+		}
+		putchar('\n');
+	}
+}
+
