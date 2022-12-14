d4c07f2ec55add1ab93ce340d44d3e0a1db34f6d
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-13 20:55:00 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - prints alphabets
+ * Description: program prints alphabets
+ * Return: void
+ */
+void times_table(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		int m;
+
+		for (m = 0; m < 10; m++)
+		{
+			if (((m * n) < 10))
+			{
+				putchar(' ');
+			}
+			printf("%d", (m *  n));
+			if (m < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+}
