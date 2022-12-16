edd7c8b699ef18bc45f83c51b140e760d70de8bd
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-16 03:33:29 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
