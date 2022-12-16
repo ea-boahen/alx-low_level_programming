8afb7f874427a5f26d9535ebc8b9a22334785e19
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-16 00:34:10 +0000

    debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - Entry point
+ * positive_or_negative - checks if n is positive or negative
  *
  * Description: "checks if n is negative or positive"
+ * 
+ * @n: feeds input to function
  *
  * Return: Always 0 (Success)
  */
-void positive_negative(int n)
+void positive_or_negative(int n)
 {
 	if (n > 0)
 		printf("%d is positive\n", n);
