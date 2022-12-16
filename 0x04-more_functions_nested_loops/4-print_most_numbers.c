c131a9e92b84f659227bd3a118d77961dad53dca
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-16 04:19:53 +0000

    task 0

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
 		{
@@ -21,5 +21,6 @@ void print_most_numbers(void)
 			_putchar(i + '0');
 		}
 	}
+
 	_putchar('\n');
 }
