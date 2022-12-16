3c563e8c78d255d19b24bf4248bfb6a84715d68d
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-16 03:59:55 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_numbers- alt function
+ * Description: prints digits from 0 to 9
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		int i = 0;
+
+		while (i < 10)
+		{
+			_putchar('0' + i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
