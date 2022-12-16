b05150d97c0a5929869efd8b4116616a79d716f9
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-16 05:11:58 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_diagonal - alt function
+ * Description: prints digits from 0 to 9
+ * @n: length of line
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			int j = 0;
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
