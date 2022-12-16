88af572c8fb1e7ea395d6f9357c79967f35a9aa4
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-16 09:33:41 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_triangle - multiplies two integers
+ * @s: size of rectangle
+ * Return: Always 0 (Success)
+ */
+void print_triangle(int s)
+{
+	int i;
+	if (s <= 0)
+		putchar('\n');
+	else
+	{
+		for (i = 1; i <= s; i++)
+		{
+			int j, k;
+
+			for (j = s - i; j > 0; j--)
+				putchar(' ');
+			for (k = 0; k < i; k++)
+				putchar('#');
+			putchar('\n');
+		}
+	}
+}
