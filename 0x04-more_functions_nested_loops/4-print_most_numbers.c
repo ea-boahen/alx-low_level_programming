60852d5e9919e528450ee68ef67d61edb84fc03e
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-16 04:17:13 +0000

    task 0

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,20 +2,23 @@
 #include <ctype.h>
 
 /**
- * print_numbers- alt function
+ * print_most_numbers- alt function
  * Description: prints digits from 0 to 9
  * Return: Always 0 (Success)
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i = 0;
 
-	while (i < 10)
+	for(i = 0; i < 10; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i == 2 || i == 4)
 		{
-			_putchar('0' + i);
-			i++;
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
 		}
 	}
 	_putchar('\n');
