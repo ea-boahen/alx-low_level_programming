55b5f45ab4ab254c9d9bba270ec402d061d8d3f0
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-20 03:58:06 +0000

    0x05 c - pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,12 @@ void puts_half(char *str)
 {
 	int x = 0;
 	int s = strlen(str);
-	int n = strlen(str) / 2;
+	int n;
+
+	if (s % 2 == 0)
+		n = strlen(str) / 2;
+	else
+		n = (strlen(str) - 1) / 2;
 
 	for (x = n; x < s; x++)
 	{
