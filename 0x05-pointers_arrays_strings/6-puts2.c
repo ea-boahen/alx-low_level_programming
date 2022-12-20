6f1443f15c1ce7e592117e69c6a1059791701bb6
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2022-12-20 03:31:29 +0000

    0x05 c - pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * puts - prints every other string but the first
+ * puts2 - prints every other string but the first
  * @str : character input to function
  *
  * Return: void.
@@ -10,6 +10,7 @@
 void puts2(char *str)
 {
 	int x = 0;
+
 	_putchar(str[x]);
 	while (str[x++])
 	{
