0740530041e6a1cd4452a09dfbaa60fd34491796
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2023-01-04 14:51:00 +0000

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - fills the first n bytes of the memory area pointed to by
+ *	s with the constant byte b
+ * @s: destination string
+ * @b: constant byte
+ * @n: number to fill to
+ *
+ * Return: void.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
