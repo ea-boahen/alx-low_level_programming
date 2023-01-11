145f1883bc4b9d1ba1a8b0fa4c321cfccb6c67d7
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2023-01-11 04:36:22 +0000

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,15 @@
 char *create_array(unsigned int size, char c)
 {
 	char *a;
+	unsigned int i
 
 	if (size == 0)
 		return (NULL);
 
 	a = malloc(sizeof(c) * size);
+	
+	for (i = 0; i < size; i++)
+		a[i] = c;
 
-	*a = c;
 	return (a);
 }
