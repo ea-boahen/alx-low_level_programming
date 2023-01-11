7d1bcf5f074476f06ce8369f237fb478caead1bb
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2023-01-11 05:53:14 +0000

    0x0B. C - malloc, free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	gridout = malloc(height * sizeof(int *));
+	a = malloc(height * sizeof(int *));
 	if (a == NULL)
 	{
 		free(a);
