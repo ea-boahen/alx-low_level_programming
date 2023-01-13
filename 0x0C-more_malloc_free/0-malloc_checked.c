e03cdf4a0ef0a9fe56a86f1984916b179d982fa5
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2023-01-13 03:33:28 +0000

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _puts_recursion- prints a string recursively
+ * @s: string to be printed recursively
+ *
+ * Return: void.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *i = malloc(b);
+
+	if (i == NULL)
+		exit (98)
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
+ */
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * string_nconcat- prints a string recursively
+ * @s1: string to be printed recursively
+ * @s2: second string
+ * @n: size to be added 
+ *
+ * Return: void.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int size = _strlen(s1) + n + 1;
+	int number = n, i = 0, j = 0;
+	char *s;
+
+	if ( s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	s = malloc(sizeof(char) * size);
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		s[i] = s1[i];
+	for (j = 0; j < number; j++)
+		s[i + j] = s2[j];
+	s[i + j] = '\0';
+	return (s);
+}
