876381da095f95af761659a5dfc300ac55fd712f
Enoch Amankwah Boahen <ea.boahen2015@gmail.com>
2023-01-19 22:20:52 +0000

    function pointer

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,24 +18,17 @@ int (*get_op_func(char *s))(int, int)
 	{ "%", op_mod },
 	{ NULL, NULL }
 	};
-	int i = 0, j = 0, k = 0;
+	int i = 0;
 
-	for (; j < 6; j++)
-	{
-		if (strcmp(s, ops[i].op) != 0)
-			k++;
-	}
-	if (k == 0)
-	{
-		printf("Error\n");
-		exit(99);
-	}
 	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);	
 		i++;
 	}
+
+	printf("Error\n");
+	exit(99);
 
 	return (0);
 }
