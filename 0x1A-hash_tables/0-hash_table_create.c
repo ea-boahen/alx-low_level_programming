c8bc862cbd3a9b41d05900db924858cdc7307ca3
ea-boahen <ea.boahen2015@gmail.com>
2023-04-19 01:26:15 +0000

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash->size = size;
-	hash->array = malloc(sizeof(hash_node_t *) *size);
+	hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
